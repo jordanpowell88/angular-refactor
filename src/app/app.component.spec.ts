65f0d9dfcfa379242246e691cff487c64a26807d
jordanpowell88 <jordan@jpdesigning.com>
2023-04-26 15:45:21 -0400

    test: add karma tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 describe('AppComponent', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+        RouterTestingModule,
+        AppComponent,
+        HttpClientTestingModule,
+      ]
     }).compileComponents();
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create the app', () => {
@@ -20,16 +27,43 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular'`, () => {
+ 
+  it('handles logout', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular');
-  });
+    app.isAuthed = true;
+    app.logout();
+    expect(app.isAuthed).toBeFalsy()
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular app is running!');
+  it('handles login', () => {
+    const username = 'jordanpowell88'
+    const testData = { message: username, status: 200 };
+    httpClient.get('/auth')
+      .subscribe(data =>
+        expect(data).toEqual(testData)
+      );
+    const req = httpTestingController.expectOne('/auth');
+    expect(req.request.method).toEqual('GET');
+    req.flush(testData);
+    httpTestingController.verify();
+  })
+
+  it('catches login errors', (done) => {
+    const error = 'Bad username or password';
+    const mockError = new ProgressEvent(error);
+
+    httpClient.get('/auth').subscribe({
+      next: () => fail('should have failed with the network error'),
+      error: (error: HttpErrorResponse) => {
+        expect(error.error).toBe(mockError);
+        done();
+      },
+    });
+
+    const req = httpTestingController.expectOne('/auth');
+
+    // Respond with mock error
+    req.error(mockError);
   });
 });
--- src/app/login-form/login-form.component.spec.ts
+++ src/app/login-form/login-form.component.spec.ts
@@ -8,7 +8,7 @@ describe('LoginFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
+      imports: [ LoginFormComponent ]
     })
     .compileComponents();
 
@@ -20,4 +20,36 @@ describe('LoginFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('emits onLogin when a username and password is present', () => {
+    const username = 'jordanpowell8';
+    const password = 'password';
+    component.username = username;
+    component.password = password;
+    const onLoginSpy = spyOn(component.onLogin, 'emit');
+    
+    component.handleFormSubmit();
+    expect(component.submitted).toBeTruthy();
+    expect(onLoginSpy).toHaveBeenCalledWith({ username, password });
+  });
+
+  it('returns without calling emit on onLogin when no username', () => {
+    const password = 'password';
+    component.password = password;
+    const onLoginSpy = spyOn(component.onLogin, 'emit');
+
+    component.handleFormSubmit();
+    expect(component.submitted).toBeTruthy();
+    expect(onLoginSpy).toHaveBeenCalledTimes(0);
+  })
+
+  it('returns without calling emit on onLogin when no password', () => {
+    const username = 'jordanpowell88';
+    component.username = username;
+    const onLoginSpy = spyOn(component.onLogin, 'emit');
+
+    component.handleFormSubmit();
+    expect(component.submitted).toBeTruthy();
+    expect(onLoginSpy).toHaveBeenCalledTimes(0);
+  })
 });
